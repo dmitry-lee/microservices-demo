90a37534975d9a6f796e6ede7e3c799f7ee47e00
dmitry-lee <dmitry.lee@yandex.ru>
2023-10-11 16:25:15 +0300

    Add app-config-data module

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaService.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaService.java
@@ -1,19 +1,20 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
-import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.config.TwitterToKafkaServiceConfigData;
 import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@ComponentScan(basePackages = "com.microservices.demo")
 public class TwitterToKafkaService implements CommandLineRunner {
 
     private final static Logger LOG = LoggerFactory.getLogger(TwitterToKafkaService.class);
